5855fe5e5fb3af93b97a80157ce3247074301a2e
Luka-Luka-Luka <luka.padovan@tutanota.de>
2026-09-12 11:09:06 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+/* Incrémentez CACHE à chaque mise en ligne d'une nouvelle version. */
+const CACHE = 'charges-v1';
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+/* Réseau d'abord, cache en secours : vous avez toujours la dernière version en ligne,
+   et l'outil continue de fonctionner hors connexion. */
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return r;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
