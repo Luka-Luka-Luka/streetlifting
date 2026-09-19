ee0a1003d88407eb11c30502a0b78cc879bacc21
Luka-Luka-Luka <luka.padovan@tutanota.de>
2026-09-19 17:51:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Incrémentez CACHE à chaque mise en ligne d'une nouvelle version. */
-const CACHE = 'charges-v18';
+const CACHE = 'charges-v20';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
